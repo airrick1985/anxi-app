054eb9dccb36d6254b10e4f1447d435dabb6459e
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-04-25 03:22:55 +0000

    更新

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 // src/api.js
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbz5lJN8Ep66o7JktZf6FYXzLOPv9KP5-ihLbSRqoBqh4RmhebjmQ3QTiCcTthhXJwg2/exec';
+const PROXY_API = 'https://vercel-proxy-api2.vercel.app/api/login';
 
 export async function loginUser(name, password) {
-  const params = new URLSearchParams({ name, password });
-  const res = await fetch(GAS_URL + '?' + params.toString());
-  return await res.json();
+  try {
+    const response = await fetch(PROXY_API, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, password })
+    });
+
+    return await response.json();
+  } catch (e) {
+    return { status: 'error', message: e.message };
+  }
 }
