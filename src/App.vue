05a5d94a49b15d9a72664aff09e85821cbbb14d7
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-04-25 03:45:03 +0000

    更新

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,18 @@
       <v-toolbar-title>ANXI 驗屋系統</v-toolbar-title>
       <v-spacer />
       <template v-if="user">
-        <v-btn icon>
+        <v-btn icon @click="dialog = true">
           <v-icon>mdi-account</v-icon>
         </v-btn>
-        <span class="me-4">{{ user.name }}</span>
+        <span class="me-4 clickable" @click="dialog = true">{{ user.name }}</span>
       </template>
     </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
+
+    <EditProfileDialog v-model:dialog="dialog" />
   </v-app>
 </template>
 
@@ -22,12 +24,23 @@
 import { storeToRefs } from 'pinia';
 import { useUserStore } from './store/user';
 import { useRouter } from 'vue-router';
+import EditProfileDialog from './components/EditProfileDialog.vue';
 
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
+const dialog = ref(false);
 
 const router = useRouter();
 if (!user.value && router.currentRoute.value.path === '/') {
   router.replace('/login');
 }
 </script>
+
+<style>
+body {
+  margin: 0;
+}
+.clickable {
+  cursor: pointer;
+}
+</style>
