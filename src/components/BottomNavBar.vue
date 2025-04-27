873e30e57e597a20b7d4ae8660d546eaddc824d5
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-04-27 09:19:22 +0000

    更新 2025-04-27 09:19:22

--- src/components/BottomNavBar.vue
+++ src/components/BottomNavBar.vue
@@ -1,30 +1,37 @@
-<!-- src/components/BottomNavBar.vue -->
 <template>
   <v-bottom-navigation
     v-model="active"
     grow
     app
   >
+    <!-- 首頁 -->
     <v-btn
       :value="0"
+      :to="{ name: 'Home' }"
+      replace
+    >
+      <v-icon size="24">mdi-home</v-icon>
+      <small>首頁</small>
+    </v-btn>
+
+    <!-- 驗屋紀錄 -->
+    <v-btn
+      :value="1"
       :to="{ name: 'InspectionRecord' }"
       replace
     >
-      <span class="d-flex flex-column align-center">
-        <v-icon size="24">mdi-home-search</v-icon>
-        <small>驗屋紀錄</small>
-      </span>
+      <v-icon size="24">mdi-home-search</v-icon>
+      <small>驗屋紀錄</small>
     </v-btn>
 
+    <!-- 紀錄總覽 -->
     <v-btn
-      :value="1"
+      :value="2"
       :to="{ name: 'InspectionOverview' }"
       replace
     >
-      <span class="d-flex flex-column align-center">
-        <v-icon size="24">mdi-view-list</v-icon>
-        <small>紀錄總覽</small>
-      </span>
+      <v-icon size="24">mdi-view-list</v-icon>
+      <small>紀錄總覽</small>
     </v-btn>
   </v-bottom-navigation>
 </template>
@@ -36,11 +43,15 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 const active = ref(0);
 
-// 當路由變更，自動切換 Tab 的選取
-watch(() => route.name, name => {
-  if (name === 'InspectionRecord') active.value = 0;
-  else if (name === 'InspectionOverview') active.value = 1;
-});
+// 當路由切換時，更新 Tab 選取狀態
+watch(
+  () => route.name,
+  (name) => {
+    if (name === 'Home') active.value = 0;
+    else if (name === 'InspectionRecord') active.value = 1;
+    else if (name === 'InspectionOverview') active.value = 2;
+  }
+);
 </script>
 
 <style scoped>
