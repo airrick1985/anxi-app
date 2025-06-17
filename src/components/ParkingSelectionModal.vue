4e252a2a883852c8dbf865fcf0dcf59678d6ddc8
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-06-17 11:16:28 +0000

    2025-06-17 19:16:28

--- src/components/ParkingSelectionModal.vue
+++ src/components/ParkingSelectionModal.vue
@@ -25,18 +25,19 @@
         <!-- 新增車位區域 -->
         <v-row align="center">
           <v-col cols="8">
-            <v-select
+       <v-select
               label="選擇可新增的車位"
               :items="availableParkingOptions"
               v-model="newParkingSelection"
               item-title="displayText"
               item-value="originalData"
+              :item-props="itemProps" 
               return-object
               hide-details
             ></v-select>
           </v-col>
           <v-col cols="4">
-            <v-btn color="primary" @click="addParking" :disabled="!newParkingSelection">新增此車位</v-btn>
+             <v-btn color="primary" @click="addParking" :disabled="!newParkingSelection">新增此車位</v-btn>
           </v-col>
         </v-row>
       </v-card-text>
@@ -74,17 +75,30 @@ watch(() => props.show, (newVal) => {
 
 const availableParkingOptions = computed(() => {
   const selectedIds = new Set(localSelectedParking.value.map(p => p['車位編號']));
-  return props.allParkingData
-    .map(p => {
-      const isSold = p['銷控狀態'] === '已售';
-      const isSelected = selectedIds.has(p['車位編號']);
-      return {
-        displayText: `${p['車位編號']}${p['銷控狀態'] ? ` (${p['銷控狀態']})` : ''}`,
-        disabled: isSold || isSelected,
-        originalData: p
-      };
-    });
+  return props.allParkingData.map(p => {
+    const isSold = p['銷控狀態'] === '已售';
+    const isSelected = selectedIds.has(p['車位編號']);
+    return {
+      // 顯示文本，例如 "B1-01 (已售)"
+      displayText: `${p['車位編號']}${p['銷控狀態'] ? ` (${p['銷控狀態']})` : ''}`,
+      // 禁用條件
+      disabled: isSold || isSelected,
+      // 原始數據
+      originalData: p
+    };
+  });
 });
+
+// ✅ 新增：定義 item-props 函數
+const itemProps = (item) => {
+  return {
+    // 將我們在 availableParkingOptions 中計算好的 disabled 屬性，
+    // 作為 prop 傳遞給 v-select 渲染出來的每一個 v-list-item
+    disabled: item.disabled,
+    // 我們也可以在這裡動態添加 class
+    class: item.disabled ? 'text-grey' : ''
+  };
+};
 
 function addParking() {
   if (newParkingSelection.value) {
