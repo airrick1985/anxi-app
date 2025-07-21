e5fe6dcd285e4a9840968e01edd5728751f123cb
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-07-21 10:59:32 +0000

    Updates

--- assets/QuoteSettings-CXNTpCk4.js
+++ assets/QuoteSettings-CXNTpCk4.js
@@ -1,4 +1,4 @@
-import{g as Ee,k as X,l as H,av as Ne,i as Se,F as de,_ as Me,z as xe,K as G,r as D,A as V,o as $,d as f,t as F,C as ke,n as Be,B as ye,c as me,w as u,b as z,a as o,ag as Ve,I as Ae,Z as Ue,ae as Oe,af as Qe,as as qe,ao as He,u as Ge,au as Ke,am as Ye,aw as je,ax as We,x as J,a2 as Ze,ay as Xe}from"./index-Ch5hQoHM.js";import{u as $e}from"./quoteStore-CatEbhWW.js";import{u as Je}from"./useSlideViewer-DWCxOpdq.js";/*!
+import{g as Ee,k as X,l as H,av as Ne,i as Se,F as de,_ as Me,z as xe,K as G,r as D,A as V,o as $,d as f,t as F,C as ke,n as Be,B as ye,c as me,w as u,b as z,a as o,ag as Ve,I as Ae,Z as Ue,ae as Oe,af as Qe,as as qe,ao as He,u as Ge,au as Ke,am as Ye,aw as je,ax as We,x as J,a2 as Ze,ay as Xe}from"./index-CsjbP0vM.js";import{u as $e}from"./quoteStore-C5HnohWc.js";import{u as Je}from"./useSlideViewer-D7saKqiC.js";/*!
  * qrcode.vue v3.6.0
  * A Vue.js component to generate QRCode. Both support Vue 2 and Vue 3
  * © 2017-PRESENT @scopewu(https://github.com/scopewu)
