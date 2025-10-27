621de50fbd9965d2caf0180f203ce933c958316d
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-10-27 07:26:43 +0000

    Release v0.0.291 - 2025-10-27 15:26:43

--- src/views/public/InspectionConsole.vue
+++ src/views/public/InspectionConsole.vue
@@ -738,7 +738,7 @@ const headers = computed(() => {
 
 // --- Methods ---
 onMounted(async () => {
- try { loadingText.value = '正在與 LINE 連接...'; await liff.init({ liffId: '2008257338-6N3jwqxA' }); //測試 2008257338-6N3jwqxA //正式 2008257338-QV34v0pb
+ try { loadingText.value = '正在與 LINE 連接...'; await liff.init({ liffId: '2008257338-QV34v0pb' }); //測試 2008257338-6N3jwqxA //正式 2008257338-QV34v0pb
  
  if (!liff.isLoggedIn()) { liff.login(); return; } loadingText.value = '正在驗證使用者權限...'; const profile = await liff.getProfile(); const success = await userStore.fetchUserByLineId(profile.userId); if (success) { isBound.value = true; loadingText.value = '正在載入建案權限...'; await projectStore.fetchProjects(); const allProjects = projectStore.projectsList; authorizedProjects.value = allProjects.filter(project => userStore.hasProjectPermission('驗屋系統', project.name)); if (props.projectId) { loadingText.value = '正在載入建案資料...'; if (!userStore.hasProjectPermission('驗屋系統', projectName.value)) { loadingText.value = '權限不足...'; isBound.value = true; isLoading.value = false; alert('權限不足'); return; } await loadProjectStructure(); await loadOptionsForChips();
  await loadData(); 
